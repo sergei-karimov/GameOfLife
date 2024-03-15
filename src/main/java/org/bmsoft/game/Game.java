3a5582a0f4b2d1e640e8a2307c73193064426fa8
Sergei Karimov <s.karimov@outlook.com>
2024-03-15 11:04:33 -0400

    [KSR-0001] Add a screen class

--- src/main/java/org/bmsoft/game/Game.java
+++ src/main/java/org/bmsoft/game/Game.java
@@ -1,29 +1,34 @@
 package org.bmsoft.game;
 
+import org.bmsoft.graphics.Screen;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferInt;
 import java.util.Random;
 
 public class Game extends Canvas implements Runnable {
     private static final long serializedVersionUID = 1L;
 
-    public static int width = 300;
-    public static int height = width / 16 * 9;
     public static int scale = 3;
+    public static int width = 1440 / scale;
+    public static int height = width / 16 * 9;
     private Thread thread;
     public JFrame frame;
     private boolean running = false;
     private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    private int[] pixels = image.getRaster().getDataBuffer().getOffsets();
+    private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+
+    private Screen screen;
 
     public Game() {
         Dimension size = new Dimension(width * scale, height * scale);
         setPreferredSize(size);
-
         frame = new JFrame();
+        screen = new Screen(width, height);
     }
 
     public synchronized void start() {
@@ -59,10 +64,15 @@ public class Game extends Canvas implements Runnable {
             return;
         }
 
+        screen.render();
+        for(int i = 0; i < pixels.length; i++) {
+            pixels[i] = screen.pixels[i];
+        }
+
         Graphics g = bs.getDrawGraphics();
-        var rnd = new Random();
-        g.setColor(Color.decode(String.valueOf(rnd.nextInt())));
+        g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
         g.dispose();
         bs.show();
     }
---
+++ src/main/java/org/bmsoft/graphics/Screen.java
@@ -0,0 +1,24 @@
+package org.bmsoft.graphics;
+
+import java.util.Random;
+
+public class Screen {
+    private final int width;
+    private final int height;
+
+    public int[] pixels;
+
+    public Screen(int width, int height) {
+        this.width = width;
+        this.height = height;
+        pixels = new int[this.width * this.height];
+    }
+
+    public void render() {
+        for (int y = 0; y < this.height; y++) {
+            for (int x = 0; x < this.width; x++) {
+                pixels[x + y * width] = new Random().nextInt();
+            }
+        }
+    }
+}
